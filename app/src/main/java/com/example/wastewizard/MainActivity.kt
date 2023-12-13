963e8e724b49f60f318fb2bc8e29b65a40a31f2a
Muhammad Ryan Al-rasyid <mryanalrasyid@gmail.com>
2023-12-13 15:07:42 +0700

    buat get started dan masuk ke halaman register signup

--- app/src/main/java/com/example/wastewizard/MainActivity.kt
+++ app/src/main/java/com/example/wastewizard/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.example.wastewizard
 
+import android.content.Intent
+import android.icu.lang.UCharacter.GraphemeClusterBreak.V
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        mau tambah register1
+        //Tombol get started
+        val btnGetstarted: Button = findViewById(R.id.btn_getstarted)
+        btnGetstarted.setOnClickListener(this)
+    }
+
+    //memberikan aksi ketika tombol get started ditekan
+    override fun onClick(v: View?) {
+        if (v != null){
+            when(v.id){
+                R.id.btn_getstarted -> {
+                    val moveIntent = Intent(this, RegisterSignup::class.java)
+                    startActivity(moveIntent)
+                }
+            }
+        }
     }
 }
