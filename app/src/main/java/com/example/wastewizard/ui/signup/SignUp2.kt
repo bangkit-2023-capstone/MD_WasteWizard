32a4bfad6be7edeabd091d8ea199436108883ac3
Annisa Nadila <nadila.nasya@gmail.com>
2023-12-18 19:00:55 +0800

    memperbaiki binding

--- app/src/main/java/com/example/wastewizard/ui/signup/SignUp2.kt
+++ app/src/main/java/com/example/wastewizard/ui/signup/SignUp2.kt
@@ -1,6 +1,5 @@
 package com.example.wastewizard.ui.signup
 
-import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
@@ -10,32 +9,25 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.example.wastewizard.R
-import com.example.wastewizard.data.UserRepository
 import com.example.wastewizard.databinding.ActivitySignUp2Binding
-import com.example.wastewizard.di.Injection
-import com.example.wastewizard.ui.ViewModelFactory
 import com.example.wastewizard.ui.login.LoginActivity
-import com.example.wastewizard.ui.login.LoginViewModel
-import com.example.wastewizard.ui.welcome.MainViewModel
 
 class SignUp2: AppCompatActivity() {
 
-    private lateinit var binding: Activity
+    private lateinit var binding: ActivitySignUp2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = .inflate(layoutInflater)
+        binding = ActivitySignUp2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setupView()
         setupAction()
 
         //Text Login
-        val txtUdhlogin2: TextView = findViewById(R.id.txt_Udhlogin2)
-        txtUdhlogin2.setOnClickListener {
+        val txtLogin: TextView = findViewById(R.id.txt_login)
+        txtLogin.setOnClickListener {
             Toast.makeText(this@SignUp2, "Text Login ditekan", Toast.LENGTH_SHORT).show()
             val moveIntent = Intent(this, LoginActivity::class.java)
             startActivity(moveIntent)
