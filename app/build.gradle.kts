a08374bd45d11c736ccb7152719ee6329a44bff7
Annisa Nadila <nadila.nasya@gmail.com>
2023-12-20 11:02:13 +0800

    mengganti text username menjadi email di activity login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,5 @@ dependencies {
     implementation ("androidx.cardview:cardview:1.0.0")
     implementation("androidx.camera:camera-view:1.3.1")
     implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.2.3")
 }
