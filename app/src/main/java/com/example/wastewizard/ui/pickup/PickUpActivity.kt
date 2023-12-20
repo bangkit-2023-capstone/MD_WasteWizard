ffcd26b190f06e558f4b08807c448002ae4cc477
Annisa Nadila <nadila.nasya@gmail.com>
2023-12-20 13:35:20 +0800

    menambahkan intent dashboard (profile, klasifikasi, dan pick up)

---
+++ app/src/main/java/com/example/wastewizard/ui/pickup/PickUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wastewizard.ui.pickup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wastewizard.R
+
+class PickUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pick_up)
+    }
+}
