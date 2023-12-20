25c8d35432361ed724e42cdc2988a66ae7ad55c8
Annisa Nadila <nadila.nasya@gmail.com>
2023-12-20 23:36:27 +0800

    menambahkan supaya bisa tetap terbuka halaman terakhir

--- app/src/main/java/com/example/wastewizard/ui/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/wastewizard/ui/viewmodel/LoginViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.wastewizard.ui.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.wastewizard.data.UserRepository
@@ -8,13 +7,11 @@ import com.example.wastewizard.data.pref.UserModel
 import kotlinx.coroutines.launch
 
 class LoginViewModel (private val repository: UserRepository) : ViewModel(){
-    fun saveSession(user: UserModel) {
+    fun saveSession(email: String, token: String) {
         viewModelScope.launch {
-            repository.saveSession(user)
+            repository.saveSession(UserModel(email, token))
         }
     }
-
-
 
     fun postLogin(email: String, password: String) = repository.userLogin(email, password)
 }
