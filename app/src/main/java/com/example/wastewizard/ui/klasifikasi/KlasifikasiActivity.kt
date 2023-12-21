b830178560f4b1c1aa7a59ce07bcf3a7cd539698
Annisa Nadila <nadila.nasya@gmail.com>
2023-12-21 10:21:17 +0800

    menambahkan intent ke galeri, camera scan, back. Kamera dan galeri sudah bisa terbuka, tapi belum bisa menampilkan gambar kembali. Untuk upload button belum dikerjakan

--- app/src/main/java/com/example/wastewizard/ui/klasifikasi/KlasifikasiActivity.kt
+++ app/src/main/java/com/example/wastewizard/ui/klasifikasi/KlasifikasiActivity.kt
@@ -1,12 +1,59 @@
 package com.example.wastewizard.ui.klasifikasi
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.example.wastewizard.R
+import com.example.wastewizard.databinding.ActivityKlasifikasiBinding
+import com.example.wastewizard.ui.main.DashboardActivity
 
 class KlasifikasiActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityKlasifikasiBinding
+    private var currentImageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_klasifikasi)
+        binding = ActivityKlasifikasiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener{
+            val intentBack = Intent(this@KlasifikasiActivity, DashboardActivity::class.java)
+            startActivity(intentBack)
+        }
+
+        binding.btnGaleri.setOnClickListener{
+            startGallery()
+        }
+        binding.btnScan.setOnClickListener{
+            startScan()
+        }
+    }
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        Toast.makeText(this, "Tombol Buka Gallery di tekan", Toast.LENGTH_SHORT).show()
+    }
+    private val launcherGallery =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri: Uri? ->
+            if (uri != null) {
+                currentImageUri = uri
+//                showImage()
+            } else {
+                Log.d("Photo Picker", "No media selected")
+            }
+        }
+    private fun startScan() {
+        currentImageUri = getImageUri(this)
+        launcherIntentCamera.launch(currentImageUri)
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+//            showImage()
+        }
     }
 }
