70cca713aeec73e87cd4c259fb4a91381fa24a9d
Annisa Nadila <nadila.nasya@gmail.com>
2023-12-28 22:15:03 +0800

    perbaiki layout

--- app/src/main/java/com/example/wastewizard/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/example/wastewizard/ui/camera/CameraActivity.kt
@@ -1,9 +1,9 @@
 package com.example.wastewizard.ui.camera
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.wastewizard.R
-
+//camera
 class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
